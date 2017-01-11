76b86511293c4b3057ac25e23d5e07cd92fadcd4
Adrian Sampson <adrian@radbox.org>
2017-01-11 11:20:28 -0500

    Look up projects by ID

--- server/server.ts
+++ server/server.ts
@@ -15,6 +15,16 @@ router.get('/', async (ctx, next) => {
   console.log(db.get('projects').value());
 });
 
+router.get('/data/projects/:id', async (ctx, next) => {
+  let id = ctx.params['id'];
+  let proj = db.get('projects').find({ id }).value();
+  if (proj) {
+    ctx.body = 'got a project';
+  } else {
+    ctx.throw('project not found', 404);
+  }
+});
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
