1691a315164cf08a079135b2411057fd533c8e10
Adrian Sampson <adrian@radbox.org>
2017-01-11 11:14:11 -0500

    Add a database, maybe?

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,18 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
+import * as LowDB from 'lowdb';
+
+const DB_NAME = 'db.json';
 
 const app = new Koa();
 const router = new Router();
+const db = new LowDB(DB_NAME);
+
+db.defaults({ projects: [] });
 
 router.get('/', async (ctx, next) => {
   ctx.body = 'hi!';
+  console.log(db.get('projects').value());
 });
 
 app.use(router.routes());
