5e165e9e31a68776c2c2b7d94c4047a44280839a
Adrian Sampson <adrian@radbox.org>
2017-01-11 10:38:02 -0500

    Add router

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,14 @@
 import * as Koa from 'koa';
+import * as Router from 'koa-router';
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx, next) => {
+router.get('/', async (ctx, next) => {
   ctx.body = 'hi!';
 });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(3000);
